ecb677705f93b3e5dcbe0974ea5688a19e794ba6
diegomdomi <diegomdomi@gmail.com>
2023-02-01 17:33:02 +0100

    adding Footer Component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,16 +6,20 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import { fontFamily, fontSize } from '@mui/system';
+import logo from '../Assets/imgLogo-removebg.png'
 export default function Footer() {
   return (
     <Box sx={{ flexGrow: 1, }} >
       <AppBar position="static" sx={{ bgcolor: '#f52246', height: 150  }}>
-      <Box justifyContent={'center'} alignItems='center' display={'flex'}
+      <Box justifyContent={'center'} alignItems='center' display={'flex'} flexDirection='column'
       sx={{
-        margin:'20px 0px'
+        margin:'40px 0px',
+        fontSize:'15px',
       }}>
-            Design & Develop 
+      <div>Design & Develop <img src={logo} alt='logo' />Diego M. Dominguez</div>
+      <div>© 2023 image_surfing</div>
+            
         </Box>
       </AppBar>
     </Box>
